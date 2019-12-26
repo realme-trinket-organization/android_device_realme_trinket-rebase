1151b6af6c0ea6ee3fa569b16812573544059d62
Gaurav Singhal <gsinghal@codeaurora.org>
2019-12-26 17:55:32 +0530

    realme_trinket: Enable build for 2.0 NFC HIDL service.
    Change-Id: I71785d876cf4e154325a72e5c51d373ea61a73e6

--- device.mk
+++ device.mk
@@ -293,7 +293,7 @@ PRODUCT_PACKAGES += \
     nqnfcee_access.xml \
     nqnfcse_access.xml \
     Tag \
-    vendor.nxp.hardware.nfc@1.2-service
+    vendor.nxp.hardware.nfc@2.0-service
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
