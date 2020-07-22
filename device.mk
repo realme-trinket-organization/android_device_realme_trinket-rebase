a80e84843bbe10bf7c6eb5a9465203512c850f77
Bharat Pawar <bpawar@codeaurora.org>
2020-07-22 20:54:03 +0530

    realme_trinket: Disable vibrator HAL interface
    Disable the existing HIDL based vibrator HAL, while
    we move to vendor vibrator HAL.
    
    Change-Id: I7529dc91c2ff994b83c7b9e7ddf4df913b9205f9

--- device.mk
+++ device.mk
@@ -445,11 +445,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
