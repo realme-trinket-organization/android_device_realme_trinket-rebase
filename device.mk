3163e5b8e34dfd7de3d0b527d5e599275809f2fb
PIPIPIG233666 <pig.priv@gmail.com>
2020-11-01 17:51:18 -0500

    realme_trinket: Split component-overrides to support pure AOSP/QTI BT stack
    Change-Id: Ia40a996ba5164ba8a6fc5dde0dfb3ef859af1477

--- device.mk
+++ device.mk
@@ -99,7 +99,8 @@ PRODUCT_PACKAGES += \
 
 # Component overrides
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/bluetooth/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+    $(DEVICE_PATH)/bluetooth/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+    $(DEVICE_PATH)/bluetooth/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
