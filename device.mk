e691d3b11b8d604717b7c9c42ef7441d48f02f95
Michael Bestas <mkbestas@lineageos.org>
2021-01-01 04:57:09 +0200

    realme_trinket: Remove android.hardware.graphics.composer@2.4-impl
    It no longer exists, the service is fully binderized
    
    Change-Id: I06ee402b50216948e0e72a2797c2edfda1c4886a

--- device.mk
+++ device.mk
@@ -120,7 +120,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.graphics.composer@2.4-impl \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
