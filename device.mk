1eaebaf19507fdababef2875da8c8a6997aad0a1
DarkJoker360 <simoespo159@gmail.com>
2021-05-16 16:23:15 +0200

    realme_trinket: Drop unneeded display interfaces

--- device.mk
+++ device.mk
@@ -142,8 +142,6 @@ PRODUCT_PACKAGES += \
 
 # Display interfaces
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.display.composer@1.0.vendor \
-    vendor.qti.hardware.display.composer@2.0.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor
 
 # DRM
