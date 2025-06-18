c78f57ebc3aad00c48732004a182560242b2d46c
dodyirawan85 <dev.irawans@gmail.com>
2025-06-19 06:35:17 +0700

    realme_trinket: rootdir: Update init scripts from `LA.UM.9.11.r1-05300-NICOBAR.0`
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -223,6 +223,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
+    init.qti.dcvs.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.oppo.rc \
