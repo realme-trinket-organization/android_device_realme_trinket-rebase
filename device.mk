8650473b0c21b29418531df9b894da9ddd6e397f
dodyirawan85 <dev.irawans@gmail.com>
2025-06-23 21:45:13 +0700

    realme_trinket: Drop useless audio effect 2.0 service
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- device.mk
+++ device.mk
@@ -27,7 +27,6 @@ DEVICE_PATH := device/realme/realme_trinket
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
