f72c72aae705d0a4ebddd6c3db5443d631fbe63b
dodyirawan85 <dev.irawans@gmail.com>
2025-06-29 19:51:30 +0700

    realme_trinket: sepolicy: Label camera properties
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,6 +186,8 @@ VENDOR_SECURITY_PATCH := 2022-09-05
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oppo/sepolicy/SEPolicy.mk
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy/vendor
 
